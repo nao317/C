82a7b9f6a56e9726026a17306cb967790ad28f30
nao317 <country.gentleman.0317@gmail.com>
2026-01-02 22:06:30 +0900

    ままこだて

---
+++ algo/algo3/array2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) a[i] = i + 1;
+    while(a.size() > 1) {
+        a.erase(a.begin());
+        a.push_back(a.front());
+        a.erase(a.begin());
+    }
+    cout << a.front() << endl;
+    return 0;
+}
