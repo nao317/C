e269313682b6656bfaab6894b4bf20a460c88f8d
nao317 <country.gentleman.0317@gmail.com>
2026-01-11 12:55:26 +0900

    ABC440 A - Octave

---
+++ atcoder/abc/abc440_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    for (int i = 0; i < y; i++) x = x * 2;
+    cout << x << endl;
+    return 0;
+}
